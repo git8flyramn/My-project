dd6a2a8108f0de5ebbc527fa97b666ff6245af37
駿 齋藤 <240063@jc-21.jp>
2025-12-09 13:22:52 +0900

    モデルの操作をしようとしている。

--- Assets/Scripts/PlayerAnim.cs
+++ Assets/Scripts/PlayerAnim.cs
@@ -17,6 +17,7 @@ public class PlayerAnim : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.UpArrow))
         {
+            
             anim.SetBool("Walk", true);
         }
         else
--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -1,17 +1,24 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
+using Unity.Mathematics;
 public class PlayerContoller : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float moveX;
+    private float moveZ;
+
     void Start()
-    {
-        
+    { 
+        //GetAxisRaw‚Å–îˆó‚ð•ûŒü‚ðŽó‚¯Žæ‚é
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveZ = Input.GetAxisRaw("Vertical");
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(moveX, 0, moveZ);
     }
 }
