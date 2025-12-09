bdafcba62cee98dfec1121d75b20f13db185696c
駿 齋藤 <240063@jc-21.jp>
2025-12-09 11:00:07 +0900

    アニメーションがキーを押すと動く機能を実装した。

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
-
+using System.Collections.Generic;
+using System.Collections;
 public class PlayerContoller : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
