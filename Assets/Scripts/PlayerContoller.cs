ef9e89272b5fc02586461d0bf0b7f4a97af2a876
駿 齋藤 <240063@jc-21.jp>
2025-12-09 13:45:36 +0900

    playerの動きが少しスムーズになった

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -1,24 +1,52 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
-using Unity.Mathematics;
 public class PlayerContoller : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private float moveX;
-    private float moveZ;
-
+   Å@CharacterController con;
+    Animator anim;
+    float speed = 3f;
+    float sprintspeed = 5f;
+    float jump = 4f;
+    float gravity = 10f;
+    Vector3 moveDirection = Vector3.zero;
+    Vector3 startPos = Vector3.zero;
     void Start()
-    { 
-        //GetAxisRawÇ≈ñÓàÛÇï˚å¸ÇéÛÇØéÊÇÈ
-        moveX = Input.GetAxisRaw("Horizontal");
-        moveZ = Input.GetAxisRaw("Vertical");
+    {
+        con = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+        startPos = transform.position;
        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(moveX, 0, moveZ);
+        float spped = Input.GetKey(KeyCode.LeftShift) ? sprintspeed : speed;
+        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 moveZ = cameraForward * Input.GetAxis("Vertical") * speed;
+        Vector3 moveX = Camera.main.transform.right * Input.GetAxis("Horizontal");
+       
+        //playerÇ™ínñ Ç…Ç¢ÇÈéû
+        if(con.isGrounded)
+        {
+            moveDirection = moveZ + moveX;
+            if(Input.GetButton("Jump"))
+            {
+                moveDirection.y = jump;
+                anim.SetBool("Jump", true);
+            }
+        }
+        else
+        {
+            moveDirection = moveZ + moveX + new Vector3(0, moveDirection.y, 0);
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+        anim.SetBool("Walk",true);
+
+        //transform.LookAt(transform.position + moveZ + moveX);
+
+        con.Move(moveDirection * Time.deltaTime);
     }
 }
