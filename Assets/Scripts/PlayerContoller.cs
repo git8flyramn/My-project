9989ba70c91cff39b83ae392438658c9c9c416d2
駿 齋藤 <240063@jc-21.jp>
2025-12-11 16:35:22 +0900

    現状の最新

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -1,37 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
-public class Anim : MonoBehaviour
+public class PlayerContoller : MonoBehaviour
 {
-    private Animator anim;
-    private CharacterController con;
-    Vector3 moveDirection = Vector3.zero;
-    Vector3 StartPos;
-     void Start()
+
+    CharacterController con;
+    Animator anim;
+    Vector3 startPos;
+    private float normalspeed = 3.0f;
+    private float sprint = 5.0f;
+    void Start()
     {
         con = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
+        startPos = transform.position; 
     }
 
     void Update()
     {
-      
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                anim.SetBool("IsRun", true);
-            }
-            else
-            {
-                anim.SetBool("IsRun", false);
-            }
-          
-            if(Input.GetKey(KeyCode.Space))
-            {
-               
-            }
-        
-      
+        float Spped = Input.GetKey(KeyCode.LeftShift) ? sprint : normalspeed;
 
+        Vector3 cameraFroward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1));
+
+        Vector3 moveZ = cameraFroward * Input.GetAxis("Vertical") * Spped;
+        Vector3 moveX = Camera.main.transform.right * Input.GetAxis("Horizontal") * Spped;
     }
 }
