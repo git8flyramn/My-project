ada3e5cfed42ce416f4ba0acb8324462a358cd4f
git8flyramn <shuntrou43@gmail.com>
2025-12-13 15:51:31 +0900

    とりあえずアニメーションの完成

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -7,10 +7,13 @@ public class PlayerContoller : MonoBehaviour
 {
 
     CharacterController con;
-    Animator anim;
+    public Animator anim;
     Vector3 startPos;
-    private float normalspeed = 3.0f;
-    private float sprint = 5.0f;
+    Vector3 moveDirection = Vector3.zero;
+    private float normalspeed = 6.0f;
+    private float sprint = 10.0f;
+    private float g = 9.8f;
+    bool IsRun;
     void Start()
     {
         con = GetComponent<CharacterController>();
@@ -20,11 +23,41 @@ public class PlayerContoller : MonoBehaviour
 
     void Update()
     {
+        Move();
+    }
+
+    void Move()
+    {
+
+        IsRun = false;
+        MoveSet();
+       
+    }
+
+    void MoveSet()
+    {
+        if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+        {
+            IsRun = true;
+           
+          
+        }
         float Spped = Input.GetKey(KeyCode.LeftShift) ? sprint : normalspeed;
-
         Vector3 cameraFroward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1));
-
         Vector3 moveZ = cameraFroward * Input.GetAxis("Vertical") * Spped;
         Vector3 moveX = Camera.main.transform.right * Input.GetAxis("Horizontal") * Spped;
+        if (con.isGrounded)
+        {
+            moveDirection = moveZ + moveX;
+
+        }
+        else
+        {
+            moveDirection = moveZ + moveX + new Vector3(0, moveDirection.y, 0);
+            moveDirection.y -= g * Time.deltaTime;
+        }
+
+        con.Move(moveDirection * Time.deltaTime);
+        anim.SetBool("IsRun", IsRun);
     }
 }
