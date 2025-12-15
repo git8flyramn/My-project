0937f9b95f0e8d82a2581c88f41ab40c251070da
駿 齋藤 <240063@jc-21.jp>
2025-12-15 13:45:38 +0900

    現状の最新

--- Assets/Scripts/TrainMove.cs
+++ Assets/Scripts/TrainMove.cs
@@ -1,14 +1,15 @@
 using System.Data;
 using UnityEngine;
-using UnityEngine.Experimental.Rendering;
-using UnityEngine.Scripting.APIUpdating;
+
 
 public class TrainMove : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
+
+
+    float moveSpeed = 0.2f;
     
-    float LimitSpeed = 30.0f;
     private Rigidbody rb;
     void Start()
     {
@@ -18,16 +19,9 @@ public class TrainMove : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        FixedUpdate();
+        transform.Translate(transform.forward * moveSpeed * Time.deltaTime,Space.World);
     }
 
-    private void FixedUpdate()
-    {
-       if(rb.angularVelocity.magnitude > LimitSpeed)
-        {
-            rb.angularVelocity = Vector3.ClampMagnitude(rb.angularVelocity, LimitSpeed);
-        }
-    }
 
 
 }
