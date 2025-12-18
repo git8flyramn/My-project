0c73780da5357822f8b566038a76a7313e130a89
駿 齋藤 <240063@jc-21.jp>
2025-12-18 11:25:38 +0900

    敵のモデルの実装

--- Assets/Scripts/TrainMove.cs
+++ Assets/Scripts/TrainMove.cs
@@ -6,24 +6,24 @@ public class TrainMove : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     
-    private Rigidbody rb;
-    Vector3 forward = Vector3.right;
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+    //private Rigidbody rb;
+    //Vector3 forward = Vector3.right;
+    //void Start()
+    //{
+    //    rb = GetComponent<Rigidbody>();
+    //}
 
     // Update is called once per frame
-    private void Update()
-    {
-        FixedUpdate();
-    }
+    //private void Update()
+    //{
+    //    FixedUpdate();
+    //}
 
-    private void FixedUpdate()
-    {
-        const int Tpower = 3;
-        rb.AddForce(forward * Tpower, ForceMode.Acceleration);
-    }
+    //private void FixedUpdate()
+    //{
+    //    const int Tpower = 5;
+    //    rb.AddForce(forward * Tpower, ForceMode.Acceleration);
+    //}
 
 
 
