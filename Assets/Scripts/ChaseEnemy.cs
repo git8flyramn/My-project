48beb14adef4f5674e34e523e9baf7d7078e5612
駿 齋藤 <240063@jc-21.jp>
2025-12-18 11:36:06 +0900

    敵は動くようになったが、アニメーションが上手くいかないのと、地面に埋まっている

--- Assets/Scripts/ChaseEnemy.cs
+++ Assets/Scripts/ChaseEnemy.cs
@@ -5,11 +5,17 @@ public class CostamerController : MonoBehaviour
 {
    
     public GameObject ObjTarget;
+     CharacterController con;
+    public Animator anim;
     private NavMeshAgent Agent;
+    Vector3 Startpos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        con = GetComponent<CharacterController>();
+        Startpos = transform.position;
     }
 
     // Update is called once per frame
