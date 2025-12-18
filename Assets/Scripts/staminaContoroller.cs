aece684a888a700432465b464dd7e3cdda8dc4f6
駿 齋藤 <240063@jc-21.jp>
2025-12-18 16:33:59 +0900

    Playerにダッシュゲージを実装している所まで

--- Assets/Scripts/staminaContoroller.cs
+++ Assets/Scripts/staminaContoroller.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
-
+using System.Collections;
+using UnityEngine.UI;
 public class StaminaContoroller : MonoBehaviour
 {
+    public float SubStamina = 3.0f;
+    public Slider slider;
+    Animator anim;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        slider = GameObject.Find("Slider").GetComponent<Slider>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            
+        }
     }
 }
