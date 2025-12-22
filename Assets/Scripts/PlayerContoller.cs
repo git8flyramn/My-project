0521f827f0ad4080d01072495b70d3ef8e19716a
駿 齋藤 <240063@jc-21.jp>
2025-12-22 15:10:05 +0900

    ゲージを減らす事が出来たが、キーを離したときに、回復されない

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -8,15 +8,12 @@ public class PlayerContoller : MonoBehaviour
 {
 
     CharacterController con;
-    private  Animator anim;
+    public  Animator anim;
     Vector3 moveDirection = Vector3.zero;
     private float normalSpeed = 6.0f;  //通常のスピード
  　 private float sprint = 10.0f; //加速したスピード
     private float g = 9.8f; //重力
-    private float stamina = 10.0f;
-    private float maxStamina = 10.0f;
-    private float statimnaCost = 3.0f;
-    private float recoverStamina = 1.5f;
+   
     Vector3 startPos = Vector3.zero;
     bool IsRun;
     void Start()
@@ -46,16 +43,15 @@ public class PlayerContoller : MonoBehaviour
             IsRun = true;
         }
         float Speed;
-        if(Input.GetKeyDown(KeyCode.LeftShift) && stamina > 1.5f)
+        if(Input.GetKey(KeyCode.LeftShift))
         {
-            stamina -= statimnaCost; 
             Speed = sprint;
         }
-        else
+        else 
         {
             Speed = normalSpeed;
         }
-
+        
         Vector3 cameraFroward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1));
         Vector3 moveZ = cameraFroward * Input.GetAxis("Vertical") * Speed;
         Vector3 moveX = Camera.main.transform.right * Input.GetAxis("Horizontal") * Speed;
@@ -70,7 +66,7 @@ public class PlayerContoller : MonoBehaviour
             moveDirection.y -= g * Time.deltaTime;
         }
         anim.SetBool("IsRun", IsRun);
-        stamina = Mathf.Min(maxStamina, stamina + recoverStamina * Time.deltaTime);
+       
         con.Move(moveDirection * Time.deltaTime);
         
     }
--- Assets/Scripts/StaminaController.cs
+++ Assets/Scripts/StaminaController.cs
@@ -1,16 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class StaminaController : MonoBehaviour
 {
+    // private float stamina = 10.0f;
+    public Slider DashGage;
+    private float CurrentStamina; // 動作の時に増減する
+    private float MaxStamina = 10.0f; //最大値(これを超えたらこの値に固定する)
+    private float Decstamina = 0.1f; //スタミナの減る量
+    private float Addstamina = 0.5f;
+  
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        CurrentStamina = MaxStamina;
+        if(DashGage != null)
+        {
+            DashGage.maxValue = MaxStamina;
+            DashGage.value = CurrentStamina;
+            StaminaUpdate();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       if(Input.GetKeyDown(KeyCode.P) && CurrentStamina > 0)
+        {
+            UseStamina(Decstamina * Time.deltaTime);
+        }
+       else if(CurrentStamina < MaxStamina)
+        {
+            RegenerateStamina(Addstamina * Time.deltaTime);
+        }
+    }
+    public void UseStamina(float dec)
+    {
+        CurrentStamina -= dec;
+        CurrentStamina = Mathf.Clamp(CurrentStamina, 0f, MaxStamina);
+        StaminaUpdate();
+    }
+
+    public void RegenerateStamina(float add)
+    {
+        CurrentStamina -= add;
+        CurrentStamina = Mathf.Clamp(CurrentStamina, 0f, MaxStamina);
+        StaminaUpdate();
+    }
+
+    private void StaminaUpdate()
+    {
+        if(DashGage != null)
+        {
+            DashGage.value = CurrentStamina;
+        }
     }
 }
