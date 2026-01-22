240d045c9a5d0290cfe0e1da9da0176bc14c678d
駿 齋藤 <240063@jc-21.jp>
2026-01-22 15:56:27 +0900

    ゴールに着いた時の画面遷移をしたい

--- Assets/Scripts/GameStartButton.cs
+++ Assets/Scripts/GameStartButton.cs
@@ -7,7 +7,7 @@ public class GameStartButton : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Invoke("StartGame", 3.0f);
+        Invoke("StartGame", 2.0f);
     }
      public void StartGame()
     {
