5b68a1ec39b3bd79ba352edfb04a414fb53c1f42
駿 齋藤 <240063@jc-21.jp>
2026-01-22 16:01:02 +0900

    ゲームクリア画面の機能の実装

---
+++ Assets/Scripts/GameClear.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class GameClear : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Invoke("ClearGame",2.0f);
+    }
+    public void ClearGame()
+    {
+        SceneManager.LoadScene("GameClear");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+   
+}
