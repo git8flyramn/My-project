6985ecb8e0ac14d628dc5fa1bb90aeb80fa111d0
駿 齋藤 <240063@jc-21.jp>
2026-01-26 11:26:58 +0900

    画面遷移のシステムの完成までもう少し

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using System.Runtime.CompilerServices;
+public class GameOver : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private string GameOverSceneName;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("train"))
+        {
+            SceneManager.LoadScene(GameOverSceneName);
+        }
+    }
+}   
