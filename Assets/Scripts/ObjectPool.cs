2f9386c4392998d09ff86761c3fce444da5f5087
git8flyramn <shuntrou43@gmail.com>
2026-02-19 21:10:11 +0900

    Assetstoreからモデルをダウンロードした

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private uint PoolSize;
+    [SerializeField] private PooledObject objectToPool;
+    private Stack<PooledObject> stack;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void SetUpPool()
+    {
+        stack = new Stack<PooledObject>();
+        PooledObject instance = null;
+        for (int i = 0; i < PoolSize; i++)
+        {
+            //ObjectToPool
+            instance = Instantiate(objectToPool);
+            instance.Pool = this;
+            instance.gameObject.SetActive(false);
+            stack.Push(instance);
+        }
+    }
+
+    public PooledObject GetPooledObject()
+    {
+       
+        if (stack.Count == 0)
+        {
+            PooledObject newInstance = Instantiate(objectToPool);
+            newInstance.Pool = this;
+            return newInstance;
+        }
+        else
+        {
+            PooledObject nextInstance = stack.Pop();
+            nextInstance.gameObject.SetActive(true);
+            return nextInstance;
+
+        }
+    }
+
+    public void ReturnToPool(PooledObject poolObject)
+    {
+        stack.Push(poolObject);
+        poolObject.gameObject.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/PooledObject.cs
@@ -0,0 +1,26 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PooledObject : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private ObjectPool pool;
+
+    public ObjectPool Pool { get => pool; set => pool = value; }
+    void Start()
+    {
+        pool = GetComponent<ObjectPool>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Release()
+    {
+        pool.ReturnToPool(this);
+    }
+}
