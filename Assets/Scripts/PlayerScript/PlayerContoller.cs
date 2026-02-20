8a9b71debd29d978d8b91db24cf0d594dcf7cdb7
git8flyramn <shuntrou43@gmail.com>
2026-02-20 21:38:07 +0900

    playerの半自動的に進む事が出来た

--- Assets/Scripts/PlayerScript/PlayerContoller.cs
+++ Assets/Scripts/PlayerScript/PlayerContoller.cs
@@ -8,14 +8,18 @@ using UnityEngine.SceneManagement;
 public class PlayerContoller : MonoBehaviour
 {
 
-   
+
     [SerializeField] ParticleSystem ParticleSystem;
     CharacterController con;
-    private  Animator anim;
+    private Animator anim;
     Vector3 moveDirection = Vector3.zero;
     private float normalSpeed = 8.0f;  //通常のスピード
- 　 private float sprint = 15.0f; //加速したスピード
-    private float g = 9.8f; //重力
+    private float sprint = 10.0f;
+    private float g = 9.8f;
+   
+
+    //  private float sprint = 15.0f; //加速したスピード
+    // private float g = 9.8f; //重力
     float Speed; //走っている時のスピード
 
     Vector3 startPos = Vector3.zero;
@@ -29,56 +33,50 @@ public class PlayerContoller : MonoBehaviour
 
     void Update()
     {
-        
-         Move();
-        
+
+        Move();
+
     }
     //playerの動き
-     void Move()
+    void Move()
     {
         IsRun = false;
         MoveSet();
     }
 
     void MoveSet()
-    {
-        //|| Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
-        {
-            
-            IsRun = true;
-            
-        }
-       
-        if(Input.GetKey(KeyCode.G))
+    { 
+        IsRun = true;
+        if (Input.GetKey(KeyCode.G))
         {
 
             //ParticleSystem.Play();
             //Debug.Log("ダッシュエフェクト再生");
             Speed = sprint;
         }
-        else 
+        else
         {
             Speed = normalSpeed;
         }
-        
-          Vector3 cameraFroward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1));
-          Vector3 moveZ = cameraFroward * Input.GetAxis("Vertical") * Speed;
-          Vector3 moveX = Camera.main.transform.right * Input.GetAxis("Horizontal") * Speed;
-        if (con.isGrounded)
+      //Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+
+        Vector3 moveZ = cameraForward * normalSpeed;
+
+        Vector3 moveX = Camera.main.transform.right * Input.GetAxis("Horizontal") * normalSpeed;
+
+        if(con.isGrounded)
         {
             moveDirection = moveZ + moveX;
-
         }
         else
         {
-           // moveDirection = moveZ + moveX + new Vector3(0,moveDirection.y, 0);
             moveDirection.y -= g * Time.deltaTime;
         }
-        anim.SetBool("IsRun", IsRun);
-       
-       con.Move(moveDirection * Time.deltaTime);
-        
+        anim.SetBool("IsRun", IsRun); con.Move(moveDirection * Time.deltaTime);
+
+
+
     }
 
    
