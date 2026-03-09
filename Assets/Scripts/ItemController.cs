f55102e554dbbf9f8eb320e2b8137802979dcbef
git8flyramn <shuntrou43@gmail.com>
2026-03-09 22:21:48 +0900

    不要なファイルの削除

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -9,7 +9,7 @@ public class ItemController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    
+    //アイテムの制御に必要な物
     public float DestroyTime;
     public float flowSpeed;
     public float RegenerateValue = 0.3f;
