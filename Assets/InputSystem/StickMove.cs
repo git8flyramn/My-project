180083d080997943ce1aae4ea517f05eee708c7b
git8flyramn <shuntrou43@gmail.com>
2026-03-12 16:37:22 +0900

    InputSystemへ変更

--- Assets/InputSystem/StickMove.cs
+++ Assets/InputSystem/StickMove.cs
@@ -4,16 +4,15 @@ using System.Collections.Generic;
 public class StickMove : MonoBehaviour
 {
 
-    private private float moveSpeed = 10f;
+     private float moveSpeed = 10f;
       void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis(Input.GetAxis("Vertical"));
-        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+      
     }
 }
