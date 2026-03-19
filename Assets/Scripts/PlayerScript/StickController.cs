4abce6ddafaddcd995024c8555d5bcdcef9b208a
git8flyramn <shuntrou43@gmail.com>
2026-03-19 11:26:15 +0900

    スティックでの移動は一応できた(con.isGroundedの時に移動しない問題)

--- Assets/Scripts/PlayerScript/StickController.cs
+++ Assets/Scripts/PlayerScript/StickController.cs
@@ -17,44 +17,34 @@ public class StickController : MonoBehaviour
     Vector3 StickDirection = Vector3.zero;
 
     private float defaultSpeed = 10.0f;//通常のスピード 
-    private float dash = 20.0f;        //ダッシュ時のスピード
+    private float dash = 15.0f;        //ダッシュ時のスピード
     private float g = 9.8f;
     private float ResetDefaultSpeed = 10.0f; //元のスピードに戻すため
     private float decStamina = 2.0f;//スタミナの減少量
 
     public  FixedJoystick StickMove;
-    int MoveSpeed = 5;
+   // int MoveSpeed = 5;
 
    // Vector3 startPos = Vector3.zero;
     bool IsRun = false;
-
+  
     void Start()
     {
         con = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
         Dash = GetComponent<DashController>();
-     　//startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+       
         MoveStick();
-
+        DashMove();
     }
 
-    void MoveStick()
+    void DashMove()
     {
-        IsRun = true;
-
-        //必要な機能
-        
-        //前に進む
-         Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
-         Vector3 moveZ = defaultSpeed * cameraForward;
-        //Vector3 moveX = Camera.main.transform.right * Input.GetAxis("Horizontal") * defaultSpeed;
-        StickDirection += this.transform.right * StickMove.Horizontal * MoveSpeed * Time.deltaTime;
-        //ダッシュ・エフェクト
         if (Input.GetKeyDown(KeyCode.G))
         {
             Dash.UseStamina(decStamina);
@@ -81,17 +71,51 @@ public class StickController : MonoBehaviour
 
             defaultSpeed = ResetDefaultSpeed;
         }
+    }
 
-        if(con.isGrounded)
+    void MoveStick()
+    {
+        //    IsRun = true;
+
+        //    //必要な機能
+
+        //    //前に進む
+
+        //    Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        //     Vector3 moveZ = defaultSpeed * cameraForward;
+        //    // StickDirection += 
+        //    //ダッシュ・エフェクト
+
+
+        //    if (con.isGrounded)
+        //    {
+        //        moveDirection = moveZ;
+
+        //    }
+        //    else
+        //    {
+        //        moveDirection.y -= g * Time.deltaTime;
+        //    }
+        //    anim.SetBool("IsRun", IsRun);
+        //    con.Move(moveDirection * Time.deltaTime);
+        IsRun = true;
+
+        Vector3 forwardMove = Vector3.forward * defaultSpeed * Time.deltaTime;
+        float horizontal = StickMove.Horizontal;
+        Vector3 side = Vector3.right * horizontal * defaultSpeed * Time.deltaTime;
+        transform.Translate(forwardMove + side);
+
+        if (con.isGrounded)
         {
-            moveDirection = StickDirection + moveZ;
+          
+
         }
         else
         {
             moveDirection.y -= g * Time.deltaTime;
         }
+       
         anim.SetBool("IsRun", IsRun);
-        con.Move(moveDirection * Time.deltaTime);
     }
   
 }
