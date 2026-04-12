26fd01d4704e5d80b316f264aac995824db62f36
git8flyramn <shuntrou43@gmail.com>
2026-04-12 11:42:52 +0900

    スタートからゴールまでのバーの作成

---
+++ Assets/Scripts/PlayerScript/ProgressBarContorller.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+public class ProgressBarContorller : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public Slider Progress;
+    private float CurrentProgress;//走るマークの進むスピード
+    private float MaxProgress = 10.0f;
+    private float decProgress = 1.0f;
+    private float MinProgress = 0.0f;
+
+    void Start()
+    {
+        CurrentProgress = MaxProgress;
+        if(Progress != null)
+        {
+            Progress.value = CurrentProgress;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void StartProgressBar()
+    {
+        CurrentProgress -= decProgress;
+        Debug.Log("バーが進んでいる");
+        CurrentProgress = Math.Clamp(CurrentProgress, MinProgress, MaxProgress);
+    }
+
+
+    private void ProgressBarUpdate()
+    {
+        if(Progress != null)
+        {
+            Progress.value = CurrentProgress;
+        }
+    }
+
+}
