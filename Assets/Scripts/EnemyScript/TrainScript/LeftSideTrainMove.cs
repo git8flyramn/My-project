73a1420379dd6fc609271b664592259bcb663856
駿 齋藤 <240063@jc-21.jp>
2026-06-26 14:13:04 +0900

    必要ないオブジェクトなどの整理

--- Assets/Scripts/EnemyScript/TrainScript/LeftSideTrainMove.cs
+++ Assets/Scripts/EnemyScript/TrainScript/LeftSideTrainMove.cs
@@ -1,22 +1,43 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 public class LeftSideTrainMove : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     private Rigidbody rb;
     private BothTrainMove BothTrain;
+    private SEManeger SE;
+    public AudioClip clip;
+    private float SeceneChangeTime = 0.5f;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         BothTrain = GetComponent<BothTrainMove>();
-       
+        SE = GetComponent<SEManeger>();
     }
 
     // Update is called once per frame
     void Update()
     {
         BothTrain.TrainMove();
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            SE.TrainAccident(clip);
+            Debug.Log("playerと左とぶつかった音を再生します");
+            StartCoroutine(TrainAciidentWaitTime());
+        }
+    }
+
+    IEnumerator TrainAciidentWaitTime()
+    {
+        yield return new WaitForSeconds(SeceneChangeTime);
+        Debug.Log("playerと左の電車がぶつかった時の判定を取りました");
+        SceneManager.LoadScene("Game Over");
     }
 }
--- Assets/Scripts/EnemyScript/TrainScript/RightSideTrainMove.cs
+++ Assets/Scripts/EnemyScript/TrainScript/RightSideTrainMove.cs
@@ -1,25 +1,46 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 public class RightSideTrainMove : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private Rigidbody rb;
     private BothTrainMove BothTrain;
+    private SEManeger SE;
+    public AudioClip clip;
+    private float SeceneChangeTIme = 0.5f;
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb        = GetComponent<Rigidbody>();
         BothTrain = GetComponent<BothTrainMove>();
-       
+        SE        = GetComponent<SEManeger>(); 
     }
 
 // Update is called once per frame
     void Update()
     {
-       
         BothTrain.TrainMove();
     }
 
-   
-   
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            SE.TrainAccident(clip);
+            Debug.Log("右の電車とplayerとぶつかった音を再生します");
+            StartCoroutine(TrainAciidentWaitTime());
+        }
+
+    }
+
+    IEnumerator TrainAciidentWaitTime()
+    {
+        yield return new WaitForSeconds(SeceneChangeTIme);
+        Debug.Log("playerと右の電車がぶつかった時の判定を取りました");
+        SceneManager.LoadScene("Game Over");
+    }
+
+
+
 }
