a02074d855c93d7e11e70fbeb27f14a9a3abbd2e
git8flyramn <shuntrou43@gmail.com>
2026-07-23 17:58:32 +0900

    現状の最新

--- Assets/Scripts/SceneScript/PlayScene/FadeOutLoad.cs
+++ Assets/Scripts/SceneScript/PlayScene/FadeOutLoad.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using System.Runtime.InteropServices;
-using UnityEditor;
 public class GameOverSceneLoad : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
