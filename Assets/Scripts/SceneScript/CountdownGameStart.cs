c05c691b9ffc5b4ad236bee81f46525185552e03
git8flyramn <shuntrou43@gmail.com>
2026-08-07 15:31:37 +0900

    カウントダウンの機能自体は出来たが、遷移する時間との間が無い事で上手く機能しない

--- Assets/Scripts/SceneScript/CountdownGameStart.cs
+++ Assets/Scripts/SceneScript/CountdownGameStart.cs
@@ -1,16 +1,52 @@
 using UnityEngine;
-
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.SceneManagement;
 public class CountdownGameStart : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    public TextMeshProUGUI CountDownText;
+    public Image ImageMask;
+    private float timer = 1.0f;
     void Start()
     {
-        
+        CountDownText.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnClickButtonStart()
+    {
+        CountdownCoroutine();
+    }
+
+    IEnumerator CountdownCoroutine()
+    {
+       
+        ImageMask.gameObject.SetActive(true);
+        CountDownText.gameObject.SetActive(true);
+
+        CountDownText.text = "3";
+        yield return new WaitForSeconds(timer);
+
+        CountDownText.text = "2";
+        yield return new WaitForSeconds(timer);
+
+        CountDownText.text = "1";
+        yield return new WaitForSeconds(timer);
+
+        CountDownText.text = "Let's Go!";
+        ImageMask.gameObject.SetActive(false);
+        CountDownText.gameObject.SetActive(false);
+
+
+
     }
 }
--- Assets/Scripts/SceneScript/PlayScene/GameStartButton.cs
+++ Assets/Scripts/SceneScript/PlayScene/GameStartButton.cs
@@ -1,23 +1,33 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine.SceneManagement;
 public class GameStartButton : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
- 
+    private CountdownGameStart countdown;
+   
+
     void Start()
     {
-     
+        countdown = GetComponent<CountdownGameStart>();
     }
      public void StartGame()
     {
-        SceneManager.LoadScene("Egorun", LoadSceneMode.Single);
+        CountdownCoroutine();
     }
     // Update is called once per frame
     void Update()
     {
         
     }
-}
+
+    IEnumerator CountdownCoroutine()
+    {
+        countdown.OnClickButtonStart();
+        Debug.Log("game Start");
+        yield return new WaitForSeconds(5.0f);
+        SceneManager.LoadScene("Egorun", LoadSceneMode.Single);
+    }
