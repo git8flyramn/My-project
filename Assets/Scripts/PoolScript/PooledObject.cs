ec9d4318f8de21c1b25801f3581e6c3de15d869c
駿 齋藤 <240063@jc-21.jp>
2026-08-28 14:44:05 +0900

    PooledObjectでobjectPoolのReturnToPool()の呼び出す部分の実装

--- Assets/Scripts/PoolScript/PooledObject.cs
+++ Assets/Scripts/PoolScript/PooledObject.cs
@@ -7,6 +7,7 @@ public class PooledObject : MonoBehaviour
 
     private ObjectPool pool;
     public ObjectPool Pool { get => pool; set => pool = value; }
+    private ObjectPool.PoolItem item;
 
     void Update()
     {
@@ -15,6 +16,7 @@ public class PooledObject : MonoBehaviour
     //生成したオブジェクトをプールに戻し、非アクティブ化
     public void Release()
     {
-        
+        pool.ReturnToPool(item.obj,item.type);
+
     }
 }
